870f55e8c8b932cff3c66fb8249cdb33611d7ba5
bruni3ra <igor.bruniera@escola.pr.gov.br>
2025-10-03 11:14:34 -0300

    78

--- js/principal.js
+++ js/principal.js
@@ -9,6 +9,7 @@ var peso = tdPeso.textContent
 console.log(paciente);
 console.log(peso);
 
+var paciente = document.querySelector("#primeiro-paciente");
 var tdAltura = paciente.querySelector(".info-altura");
 var peso = tdAltura.textContent
 console.log(paciente);
