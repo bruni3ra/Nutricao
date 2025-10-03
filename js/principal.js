ffe2eb9dad7c382fd2b317568cbaf214a9c7b4a4
bruni3ra <igor.bruniera@escola.pr.gov.br>
2025-10-03 10:46:25 -0300

    567

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,7 @@
  var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("Aparecida Nutricionista")
+titulo.textContent = ("Aparecida Nutricionista");
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
